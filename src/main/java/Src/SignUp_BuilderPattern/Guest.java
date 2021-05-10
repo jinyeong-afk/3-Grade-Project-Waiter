6b2aa280971f3dee994bd9772719d5377ebfaae6
PARK Sungho <56144682+Hoouoo@users.noreply.github.com>
2021-05-10 23:04:50 +0900

    Add files via upload

---
+++ src/main/java/Src/SignUp_BuilderPattern/Guest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Src.SignUp_BuilderPattern;
+
+/**
+ *
+ * @author 박성호
+ */
+public class Guest extends Member{
+    
+    public Guest(int idx, String id, String pw, String name, String tel, String address, int balance){
+        this.idx = idx;
+        this.id = id;
+        this.pw = pw;
+        this.name = name;
+        this.tel = tel;
+        this.address = address;
+        this.balance = balance;
+    }
+}
